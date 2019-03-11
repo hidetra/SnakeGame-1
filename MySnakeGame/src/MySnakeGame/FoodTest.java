3606f1ab4e254a76acd056b0e429877093383946
hidetra <48260817+hidetra@users.noreply.github.com>
2019-03-11 13:11:34 -0700

    Add files via upload

---
+++ MySnakeGame/src/MySnakeGame/FoodTest.java
@@ -0,0 +1,14 @@
+package MySnakeGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FoodTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ MySnakeGame/src/MySnakeGame/SnakeTest.java
@@ -0,0 +1,14 @@
+package MySnakeGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SnakeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
